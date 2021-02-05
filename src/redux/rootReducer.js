66c0cce62cb3668dfc49b93dcbd238f242004c09
Devilzer <jenadeepak724@gmail.com>
2021-02-05 23:25:45 +0530

    add redux structure

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,20 @@
+import userReducer from "./reducers/userReducer";
+import dishReducer from "./reducers/dishReducer";
+import { combineReducers } from "redux";
+
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+    user : userReducer,
+    dish : dishReducer
+});
+
+const persistConfig = {
+    key : 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+export default rootReducer;
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./rootReducer";
+import { persistStore } from "redux-persist";
+
+const store = createStore(rootReducer,composeWithDevTools());
+const persistor = persistStore(store);
+
+export {store, persistor};
